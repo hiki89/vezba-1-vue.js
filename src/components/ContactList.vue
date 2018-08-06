ec3efbb7c168c6f767f8d536956265a0c0323b58
hiki89 <markohitl89@gmail.com>
2018-08-06 20:10:54 +0200

    task 2

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,11 +1,33 @@
 <template>
   <div >
     <h1>Contact List</h1>
+    <table align="center">
+        <thead>
+            <th>Name</th>
+            <th>Email</th>
+        </thead>
+        <tbody>
+            <tr v-for="(contact, key) in contacts" :key="key">
+               <td>{{contact.name}}</td> 
+               <td>{{contact.email}}</td> 
+            </tr>
+        </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ContactList'
+  name: 'ContactList',
+  data() {
+      return {
+          contacts: [
+              {name:"John", email:"example1@gmail.com"},
+              {name:"Jack", email:"example2@gmail.com"},
+              {name:"Susan", email:"example3@gmail.com"},
+              {name:"Jane", email:"example4@gmail.com"}
+          ]
+      };
+  }
 }
 </script>
