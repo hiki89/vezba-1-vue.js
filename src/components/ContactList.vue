75ceac5b82ca15fda124fcb1da62aa834efa6ad1
hiki89 <markohitl89@gmail.com>
2018-08-06 20:39:32 +0200

    task 3

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,6 +1,13 @@
 <template>
   <div >
     <h1>Contact List</h1>
+    <form @submit.prevent>
+        <label>Name</label>
+        <input v-model="newContact.name" type="text" placeholder="Name"> <br>
+        <label>Email</label>
+        <input v-model="newContact.email" type="text" placeholder="Email"> <br>
+        <button @click="addContact" type="submit">Add Contact</button>
+    </form>
     <table align="center">
         <thead>
             <th>Name</th>
@@ -26,8 +33,17 @@ export default {
               {name:"Jack", email:"example2@gmail.com"},
               {name:"Susan", email:"example3@gmail.com"},
               {name:"Jane", email:"example4@gmail.com"}
-          ]
+          ],
+          newContact: {
+
+          }
       };
+  },
+  methods: {
+      addContact() {
+          this.contacts.push(this.newContact);
+          this.newContact = {}
+      }
   }
 }
 </script>
